99983a473b7d040c935d2b5c8cbbd75e5f4b7c9d
Francois Wavresky <fwavresky@humaxdigital.com>
2016-07-09 19:13:26 +0900

    adding forum API

--- src/Routers/APIs.js
+++ src/Routers/APIs.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import articleAPI from './articleAPI';
 import galleryAPI from './galleryAPI';
+import forumAPI from './forumAPI';
 import userController from './../controller/userController'
 let user_contro = new userController();
 
@@ -9,6 +10,7 @@ module.exports = function (app) {
     app.use(express.static('./src/public'));
     app.use('/articles', articleAPI);
     app.use('/photos', galleryAPI);
+    app.use('/forum', forumAPI);
 
     app.route('/signup')
         .post(user_contro.signup);
