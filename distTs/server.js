81eaa00a42b582cc3d727e7b76fa306379ed89bc
Ethan <jcdby@hotmail.com>
2016-07-09 12:34:57 +0900

    solved every typings problem.

--- distTs/server.js
+++ distTs/server.js
@@ -2,13 +2,13 @@
 var express = require('express');
 var APIs_1 = require('./src/Routers/APIs');
 var CORSmiddleware_1 = require('./src/Middlewares/CORSmiddleware');
-var body_parser_1 = require('body-parser');
-var multer_1 = require('multer');
+var bodyParser = require('body-parser');
+var multer = require('multer');
 var app = express();
-var upload = multer_1.default();
+var upload = multer();
 app.use(CORSmiddleware_1.allowCrossDomain);
-app.use(body_parser_1.default.json());
-app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 APIs_1.default(app);
 app.listen(10000, function () {
     console.log('SuWon church API Server listening on port 10000!');
