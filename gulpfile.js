f95fed71d584b034a11f99e220205672a7e4683b
Ethan <jcdby@hotmail.com>
2016-07-23 17:10:39 +0900

    fixed gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('./gulp_repo')();
 
-gulp.task('default', ['ts-watch']);
+gulp.task('default', ['ts-compile', 'ts-watch']);
 gulp.task('server', ['server-watch']);
 gulp.task('dev', ['ts-watch', 'server']);
